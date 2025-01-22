b5e81379a71b5ebc4f60e941cfdd5e0e156cf5e2
jungsick kim <kim.jungsick@sk.com>
2025-01-22 10:24:09 +0900

    clean code

--- app/src/main/java/com/skb/btvplus/navigator/LandingItem.kt
+++ app/src/main/java/com/skb/btvplus/navigator/LandingItem.kt
@@ -1,8 +1,6 @@
 package com.skb.btvplus.navigator
 
-import com.google.gson.Gson
-
 data class LandingItem(
     var id: String? = null,
-    val name: String? = null
+    val name: String? = null,
 )
