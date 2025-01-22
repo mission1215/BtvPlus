d419546a05f7ab038c32b29632456c422952be92
jungsick kim <kim.jungsick@sk.com>
2025-01-22 10:24:01 +0900

    clean code

--- app/src/main/java/com/skb/btvplus/navigator/LandingItem.kt
+++ app/src/main/java/com/skb/btvplus/navigator/LandingItem.kt
@@ -5,14 +5,4 @@ import com.google.gson.Gson
 data class LandingItem(
     var id: String? = null,
     val name: String? = null
-) {
-    fun toJson(): String {
-        return Gson().toJson(this)
-    }
-
-    companion object {
-        fun fromJson(json: String): LandingItem {
-            return Gson().fromJson(json, LandingItem::class.java)
-        }
-    }
-}
+)
