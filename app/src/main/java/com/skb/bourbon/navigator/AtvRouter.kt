cd8143a84c9406984b3b7c05cb6559d99a3aff0f
jungsick kim <kim.jungsick@sk.com>
2025-01-23 14:57:32 +0900

    코드 정리

--- app/src/main/java/com/skb/bourbon/navigator/AtvRouter.kt
+++ app/src/main/java/com/skb/bourbon/navigator/AtvRouter.kt
@@ -2,17 +2,6 @@ package com.skb.bourbon.navigator
 
 import androidx.navigation.NavHostController
 import com.skb.bourbon.main.BaseNavItems
-
-/**
- * Landing view type
- *
- * @constructor Create empty Landing view type
- */
-sealed class LandingViewType(){
-    object Home : LandingViewType()
-    object Detail : LandingViewType()
-}
-
 /**
  * Navigate to home
  *
